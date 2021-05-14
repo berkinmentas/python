9b593d66e5287e8939a21166b66c4d563c285a05
berkinmentas <83673094+berkinmentas@users.noreply.github.com>
2021-05-14 22:44:18 +0300

    Add files via upload

--- arayüz.py
+++ arayüz.py
@@ -1,4 +1,9 @@
 from tkinter import *
+import sqlite3
+
+con = sqlite3.connect("rehber.db")
+imlec = con.cursor()
+
 pencere = Tk()
 pencere.title("Telefon Rehberi")
 pencere.geometry("700x350")
@@ -36,14 +41,32 @@ metin5.place(x=300, y=25)
 giris5 = Entry(width=25)
 giris5.place(x=450,y=28)
 
+liste = Listbox(width=60)
+liste.place(x=300, y=170)
 
-buton = Button(text=("Kaydet"), fg=("Black"), bg=("Yellow"),width=15)
+def degerekle():
+    ad = giris.get()
+    no = giris2.get()
+    detay = giris4.get()
+    liste.insert(END, (ad, no, detay))
+    def kayitekle():
+        imlec.execute("""INSERT INTO kisiler VALUES (?, ?, ?)""", [ad, no, detay])
+        con.commit()
+    kayitekle()
+def degersil():
+    sil = giris.get()
+    liste.delete(ACTIVE)
+    def kayitsil():
+        imlec.execute("""DELETE FROM kisiler WHERE isim = ?""", [sil])
+        con.commit()
+    kayitsil()
+buton = Button(text=("Kaydet"), fg=("Black"), bg=("Yellow"),width=15, command=degerekle)
 buton.place(x=150, y=150)
 
-buton2 = Button(text=("Yeni kayıt ekle"),fg=("Black"), bg=("Yellow"),width=15)
+buton2 = Button(text=("Yeni kayıt ekle"),fg=("Black"), bg=("Yellow"),width=15, command=degerekle)
 buton2.place(x=1, y=180)
 
-buton3 = Button(text=("Kayıt Sil"),fg=("Black"), bg=("Yellow"), width=15)
+buton3 = Button(text=("Kayıt Sil"),fg=("Black"), bg=("Yellow"), width=15, command=degersil)
 buton3.place(x=1, y=210)
 
 buton4 = Button(text=("Kaydı Düzenle"),fg=("Black"), bg=("Yellow"), width=15)
@@ -58,8 +81,7 @@ buton6.place(x=1, y=300)
 buton7 = Button(text=("Gözat"),fg=("Black"), bg=("Yellow"))
 buton7.place(x=275, y=92)
 
-liste = Listbox(width=60)
-liste.place(x=300, y=170)
+
 
 
 pencere.mainloop()
